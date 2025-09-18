2f3221759e55781b4b977fd9de05841c2336828d
Hitesh Dadabhau Chaudhari <hiteshchaudhari0023@gmail.com>
2025-09-18 18:15:08 +0530

    Day_16

---
+++ Day_16/app.js
@@ -0,0 +1,54 @@
+// let msg = "    Hello     "
+// msg.trim
+// console.log(msg.trim)
+
+// let password = prompt("set your password");
+// let newpass = password.trim();
+// console.log(newpass);
+// let msgg = "          Hitesh Chaudahri      "
+// let str = msgg.trim()
+// console.log(str)
+
+
+// let name = "Hitesh Chaudahari"
+// let msg = "errror"
+// console.log(name.toUpperCase());
+// console.log(msg.toUpperCase());
+// console.log(name.toLowerCase());
+
+
+// let str = "I love coding"
+// str.indexOf("love")
+// str.indexOf("j")
+// str.indexOf("o")
+// console.log(str)
+
+// let vari = "       Hello     "
+// let newMsg = vari.trim();
+// console.log("aftre trim : ",newMsg);
+// newMsg = newMsg.toUpperCase();
+// console.log("after uppercase : ",newMsg)
+// let newMsg = vari.trim().toUpperCase();
+// console.log(newMsg)
+
+
+
+// let msg = "HiteshChaudahri"
+// console.log(msg.slice(5,msg.length));
+// console.log(msg.slice(-3));
+
+// console.log(msg.replace("Hitesh","Mahesh"));
+// console.log(msg.repeat(3))
+    
+
+// let msg = "hello!";
+// console.log(msg.trim().toUpperCase())
+
+
+// let name = "ApnaCollege"
+// console.log(name.slice(4,9));
+// console.log(name.indexOf("na"));
+// console.log(name.replace("Apna","Our"));
+// console.log(name.slice(4).replace('l','t'));
+// let newstr = name.slice(4).replace('l','t').replace("l",'t')
+// console.log(newstr)
---
+++ Day_16/array.js
@@ -0,0 +1,40 @@
+let students = ["Hitesh","Mahesh","Durgesh"];
+
+let nums = [2,4,6,8];
+console.log(nums)
+let name = "hitesh"
+console.log(name[0]);
+console.log(nums[1]);
+console.log(nums.length);
+console.log(typeof nums);
+let info =["Hitesh",20,5.8]
+console.log(info)
+let empty = [];
+console.log(empty)
+
+let fruits = ["Mango","Grapes","orange"];
+console.log(fruits);
+fruits[0]= "Banana";
+console.log(fruits);
+console.log(fruits[10]="Papaya" ,fruits);
+
+let cars = ["odi","bmw","maruti","XUV"]
+console.log(cars)
+cars.push("Toyota")
+console.log(cars)
+cars.unshift("Jeep");
+console.log(cars)
+cars.pop();
+console.log(cars)
+cars.shift();
+console.log(cars)
+
+let months = ["Junuary","july","march","august"];
+console.log(months);
+months.shift("Junuary");
+months.unshift("July");
+
+
+months[1]="june";
+console.log(months);
+
