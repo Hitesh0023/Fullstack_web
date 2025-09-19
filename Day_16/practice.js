105bb99dd16cec327076bb0de145d6250e6b0a60
Hitesh Dadabhau Chaudhari <hiteshchaudhari0023@gmail.com>
2025-09-19 19:11:19 +0530

    Day_16

---
+++ Day_16/practice.js
@@ -0,0 +1,37 @@
+//  
+
+// let str = prompt("Enter the string : ");
+// if(str == 0){
+//     console.log("the string is  empty");
+// }
+// else{
+//     console.log("the string is not empty")
+// }
+
+
+// let str = ("HitEsh");
+// let idx = 3;
+// if(str[idx] == str[idx].toLowerCase()){
+//     console.log("Character is lowercase");
+// }
+// else{
+//     console.log("character is not lowercase");
+// }
+
+
+
+// let str = prompt("please enter the string")
+// console.log(`original string = ${str}`);
+// console.log(`string without spaces = ${str.trim()}`);
+
+
+
+let arr = [7,9,3,-4,5,1];
+let item = 55;
+if(arr.indexOf(item)!=-1){
+    console.log("element exists in array");
+
+}
+else{
+    console.log("element dosen't exists in array");
+}
