267f0f05907a5a97e2a36975aea33de13a07d16a
Hitesh Dadabhau Chaudhari <hiteshchaudhari0023@gmail.com>
2025-09-24 03:23:13 +0530

    Day_20

---
+++ Day_20/app.js
@@ -0,0 +1,105 @@
+// const student = {
+//     name : "Hitesh",
+//     age: 23,
+//     eng: 95,
+//     phy : 97,
+//     math : 86,
+//     getAvg(){
+//         console.log(this);
+//         let avg = (this.eng + this.math + this.phy)/3;
+//         console.log(`${this.name} got average marks = ${avg}`);
+    
+// }
+// }
+// function getAvg(){
+//     console.log(this);
+// }
+
+// console.log("hello")
+// console.log("hello")
+// // let a = 5
+// try{
+//     console.log(a)
+// } catch(err){
+//     console.log("caught an error ....a is not defined");
+//     console.log(err);
+// }
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+
+
+// const sum = (a,b) => {
+//     console.log(a+b)
+
+
+// const cube = n => {
+//     return n*n*n;
+// };
+
+// const pow = (a,b) => {
+//     return a**b;
+// }
+
+// const hello = () =>{
+//     console.log("Hello world!")
+// } 
+
+// const sum =(a,b) => (a+b);
+
+// const mul = (a,b) =>  a*b;
+
+// const cube = (n) => n*n*n   
+
+
+// console.log("Hi there!");
+
+// setTimeout(() => {
+//     console.log("Apna college");
+// },4000);
+
+// console.log("Welcome to")
+
+// let id = setInterval(() =>{
+//     console.log("Apna college");
+
+// },2000);
+// console.log(id)
+
+
+// const student = {
+//     name: "Hitesh",
+//     marks: 95,
+//     prop : this,
+//     getname : function(){
+//         console.log(this);
+//         return this.name;
+//     },
+//     getMarks: () =>{
+//         console.log(this);
+//         return this.marks;
+//     },
+//     getInfo1:function () {
+//         setTimeout( () => {
+//             console.log(this);
+//         },2000);
+//     },
+//     getInfo2: function () {
+//         setTimeout( function (){
+//             console.log(this);
+//         },2000);
+//     },
+// };
+
+
+
+// const square = (n) => n**2;
+
+// let id = setInterval( () =>{
+//     console.log("Hello World!");
+// },2000);
+
+// setTimeout(() => {
+//     clearInterval(id);
+//     console.log("clear interval ran")
+// },10000);
