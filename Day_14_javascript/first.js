954890077ba70d3dead8b3904ff7637cbdb79dbe
Hitesh Dadabhau Chaudhari <hiteshchaudhari0023@gmail.com>
2025-09-27 23:03:48 +0530

    Day_14

---
+++ Day_14_javascript/first.js
@@ -0,0 +1,11 @@
+'pconsole.log("Hello from external JavaScript!");
+alert("This message is from the external JS file.");
+let a = 56;
+let b = 55;
+console.log(b+a)
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(5+6);
+console.log(a%b);
+console.log(2**3);
---
+++ Day_14_javascript/operators.js
@@ -0,0 +1,11 @@
+let a= prompt("Enter the number :")
+let b = prompt("Enter the secind number :")
+
+console.log(a+b);
+console.log(a!=b);
+console.log(a==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a===b);
+console.log(a<b && a!=b)
+console.log(a==b || a!=b)
