85ee2e96afeae38966c480d3a02fad9fb914f96e
Hitesh Dadabhau Chaudhari <hiteshchaudhari0023@gmail.com>
2025-10-07 20:37:17 +0530

    Day_28_OOP

--- Day_28_OOP/backend/index.js
+++ Day_28_OOP/backend/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const app = express();
+const port = 8000;
+
+
+app.get("/register", (req, res) => {
+    let { user, password } = req.query;
+    res.send(`Standard get response: Welcome ${user}`);
+});
+app.post("/register", (req, res) => {
+    let { user, password } = req.body;
+    res.send(`Standard post response: ${user}, ${password}`);
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
