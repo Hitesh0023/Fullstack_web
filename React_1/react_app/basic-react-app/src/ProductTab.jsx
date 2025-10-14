2b35a5db40f03ab5ec26cb5e6610f6ec611c4d3d
Hitesh Dadabhau Chaudhari <hiteshchaudhari0023@gmail.com>
2025-10-14 22:29:13 +0530

    React

--- React_1/react_app/basic-react-app/src/ProductTab.jsx
+++ React_1/react_app/basic-react-app/src/ProductTab.jsx
@@ -1,11 +1,13 @@
 import Product from "./product.jsx";
 
 function ProductTab(){
+    let features = ["Good Camera", "Long Battery Life", "Fast Processor"];
+    let features2 = ["Warranty","EMI","Service"]
     return (
         <>
-        <Product/>
-        <Product/>
-        <Product/>
+        <Product title="Phone" price={60000/2} features={features} features2={features2} />
+        <Product title="Tablet" price={40000} features={features} features2={"gift"} />
+        <Product title="Laptop" price={85000} features={features} />
         </>
     );
 }
--- React_1/react_app/basic-react-app/src/product.jsx
+++ React_1/react_app/basic-react-app/src/product.jsx
@@ -1,8 +1,14 @@
 import "./Product.css";
-function Product(){
-    return <div className="Product">
-        <h1>Product Title</h1>
-        <h3>Product Description</h3>
+function Product({title, price , features , features2}) {
+    console.log(features)
+    // console.log(props.title);
+    return (
+    <div className="Product">
+        <h1>{title}</h1>
+        <h3>Price : {price}</h3>
+        <p>Features : {features}</p>
+        <p>Features2 : {features2}</p>
     </div>
+    );
 }
 export default Product;
