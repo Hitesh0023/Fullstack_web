6131a8ada8fab2221e8ef26d91e3ce6c46fc8d71
Hitesh Dadabhau Chaudhari <hiteshchaudhari0023@gmail.com>
2025-10-15 22:22:35 +0530

    React

--- React_1/react_app/basic-react-app/src/ProductTab.jsx
+++ React_1/react_app/basic-react-app/src/ProductTab.jsx
@@ -1,13 +1,13 @@
 import Product from "./product.jsx";
 
 function ProductTab(){
-    let features = ["Good Camera", "Long Battery Life", "Fast Processor"];
-    let features2 = ["Warranty","EMI","Service"]
+    // let features = [<li>"Good Camera"</li>, <li>"Long Battery Life"</li>, <li>"Fast Processor"</li>];
+    // let features2 = ["Warranty","EMI","Service"]
     return (
         <>
-        <Product title="Phone" price={60000/2} features={features} features2={features2} />
-        <Product title="Tablet" price={40000} features={features} features2={"gift"} />
-        <Product title="Laptop" price={85000} features={features} />
+        <Product title="Phone" price={60000/2}  />
+        <Product title="Tablet" price={40000}  />
+        <Product title="Laptop" price={85000}  />
         </>
     );
 }
--- React_1/react_app/basic-react-app/src/product.jsx
+++ React_1/react_app/basic-react-app/src/product.jsx
@@ -1,14 +1,29 @@
 import "./Product.css";
-function Product({title, price , features , features2}) {
-    console.log(features)
-    // console.log(props.title);
+function Product({ title, price }) {
+  // const list = features.map((feature, index) => <li key={index}>{feature}</li>);
+  // console.log(features)
+  // console.log(props.title);
+//   if (price > 30000) {
     return (
-    <div className="Product">
+      <div className="Product">
         <h1>{title}</h1>
         <h3>Price : {price}</h3>
-        <p>Features : {features}</p>
-        <p>Features2 : {features2}</p>
-    </div>
+        {price > 30000 ? <p>Discount of 5%</p> : <a href="#">No Discount</a>}
+        {/* <p>Features : {list}</p> */}
+        {/* <p>Features2 : {features2}</p> */}
+      </div>
     );
 }
+//   } else {
+//     return (
+//       <div className="Product">
+//         <h1>{title}</h1>
+//         <h3>Price : {price}</h3>
+//         {/* <h3 style={{color:"red"}}>Not Available</h3> */}
+//         {/* <p>Features : {list}</p> */}
+//         {/* <p>Features2 : {features2}</p> */}
+//       </div>
+//     );
+//   }
+
 export default Product;
